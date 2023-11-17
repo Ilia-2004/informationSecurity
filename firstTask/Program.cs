a0664a63e704b4440e75350e5c6575c2730d80bc
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-17 23:02:57 +0400

    Added a dictionary of alphabet to first task

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace informationSecurity
+﻿using System;
+using System.Collections.Generic;
+
+namespace informationSecurity
 {
   internal abstract class Program
   {
     public static void Main(string[] args)
     {
+      Dictionary<char, string> alphabet = new Dictionary<char, string>()
+      {
+        {'А', "ме"}, {'Б', "ли"}, {'В', "ко"}, {'Г', "ин"}, {'Д', "зе"},
+        {'Е', "жу"}, {'Ж', "ню"}, {'З', "ою"}, {'И', "пы"}, {'К', "ра"},
+        {'Л', "су"}, {'М', "ти"}, {'Н', "у"},  {'О', "хи"}, {'П', "от"},
+        {'Р', "ца"}, {'С', "чу"}, {'Т', "ше"}, {'У', "ам"}, {'Ф', "ик"},
+        {'Х', "ъ"},  {'Ц', "то"}, {'Ч', "ь"},  {'Ш', "ю"},  {'Щ', "я"},
+        {'Ъ', "ф"},  {'Ы', "а"},  {'Ь', "бе"}, {'Э', "за"}, {'Ю', "гу"},
+        {'Я', "ди"}
+      };
     }
   }
 }
