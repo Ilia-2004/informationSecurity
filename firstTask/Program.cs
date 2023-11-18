e561394b7dbef9b3f3832c5d4556e469147bf872
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-18 12:51:06 +0400

    Added files and methods to the project

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -5,18 +5,25 @@ namespace informationSecurity
 {
   internal abstract class Program
   {
+    // encryption method
+    private string _encryptionMethod()
+    {
+
+      return "str";
+    }
+
+    // decryption method
+    private string _decryptionMehtod() 
+    {
+
+      return "str";
+    }
+
+    // main method 
     public static void Main(string[] args)
     {
-      Dictionary<char, string> alphabet = new Dictionary<char, string>()
-      {
-        {'А', "ме"}, {'Б', "ли"}, {'В', "ко"}, {'Г', "ин"}, {'Д', "зе"},
-        {'Е', "жу"}, {'Ж', "ню"}, {'З', "ою"}, {'И', "пы"}, {'К', "ра"},
-        {'Л', "су"}, {'М', "ти"}, {'Н', "у"},  {'О', "хи"}, {'П', "от"},
-        {'Р', "ца"}, {'С', "чу"}, {'Т', "ше"}, {'У', "ам"}, {'Ф', "ик"},
-        {'Х', "ъ"},  {'Ц', "то"}, {'Ч', "ь"},  {'Ш', "ю"},  {'Щ', "я"},
-        {'Ъ', "ф"},  {'Ы', "а"},  {'Ь', "бе"}, {'Э', "за"}, {'Ю', "гу"},
-        {'Я', "ди"}
-      };
+
+      
     }
   }
 }
