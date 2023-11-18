4a3c393d4f59de37d3312bf6c7a1cd6572d8b14f
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-18 13:14:48 +0400

    Added the first solution to the methods

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -1,29 +1,38 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace informationSecurity
 {
   internal abstract class Program
   {
+    private static string _path = @".\Contents\Key.txt";
     // encryption method
-    private string _encryptionMethod()
+    private static string _encryptionMethod()
     {
+      var alphabet = new Dictionary<char, string>();
 
+      using (FileStream fstream = File.OpenRead(_path))
+      {
+        var strKey = fstream;
+        Console.WriteLine(strKey);
+      }
       return "str";
     }
 
     // decryption method
-    private string _decryptionMehtod() 
+    private static string _decryptionMehtod() 
     {
-
+        
       return "str";
     }
 
     // main method 
     public static void Main(string[] args)
     {
+       _encryptionMethod();
 
-      
+       Console.WriteLine("Hello");
     }
   }
 }
