d0ea6f94a8f643b9408b66bea98b5aa34d1ee81d
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-20 01:00:01 +0400

    Added for the second task

--- secondTask/Program.cs
+++ secondTask/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace secondTask
+﻿using System;
+
+namespace secondTask
 {
   internal abstract class Program
   {
     public static void Main(string[] args)
     {
+      Console.Write("Введите строку: ");
+      var input = Console.ReadLine();
+
+      // Переворачиваем строку
+      var reversed = input.ToCharArray();
+      Array.Reverse(reversed);
+      var reversedString = new string(reversed);
+
+      // Разбиваем строку на блоки по 6 символов
+      var length = reversedString.Length;
+      for (var i = 0; i < length; i += 6)
+      {
+        var remaining = Math.Min(6, length - i);
+        var block = reversedString.Substring(i, remaining);
+        Console.WriteLine(block);
+      }
     }
   }
 }
