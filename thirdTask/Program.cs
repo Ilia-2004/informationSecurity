7b9a14fcd093e73ea60ac056349b3e10d5f7cb91
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-11-20 13:56:45 +0300

    Transfered of the solution

--- thirdTask/Program.cs
+++ thirdTask/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace thirdTask
+﻿using System.IO;
+using System;
+
+namespace thirdTask
 {
   internal abstract class Program
   {
+    private const string Path = @".\Contents\";
     public static void Main(string[] args)
     {
-    }
+             // files path
+
+            //Console.Write("Введите строку: ");
+            //var input = Console.ReadLine();
+            var input = string.Empty;
+            var strOut = string.Empty;
+            using (var stream = new StreamReader($"{Path}Input.txt"))
+            {
+                input = stream.ReadToEnd();
+                //arrKey = strKey.Split(',');
+            }
+
+            // Переворачиваем строку
+            var reversed = input.ToCharArray();
+            Array.Reverse(reversed);
+            var reversedString = new string(reversed);
+
+            // Разбиваем строку на блоки по 6 символов
+            var length = reversedString.Length;
+            for (var i = 0; i < length; i += 6)
+            {
+                var remaining = Math.Min(6, length - i);
+                var block = reversedString.Substring(i, remaining);
+                Console.WriteLine(block);
+            }
+        }
   }
 }
