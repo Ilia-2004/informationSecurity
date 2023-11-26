84823e985e6c75c336bbdf0e0871f21cb1c77cff
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-26 15:21:39 +0400

    Updated the methods in the second solution

--- secondTask/Program.cs
+++ secondTask/Program.cs
@@ -15,11 +15,19 @@ namespace secondTask
     // the method of calculating the frequency of letters in the text
     private static Dictionary<string, double> s_frequencyMethod(string fileName) 
     {
-      // переменные
-      var contentInput = File.ReadAllText($"{Path}{fileName}");     // чтение исходного файла
-      var lengthContentInput = contentInput.Length;                       // длина исходного файла
-      var countingContentInput = new Dictionary<string, double>();           // список букв и их количество
-      var countingFrequencyContentInput = new Dictionary<string, double>();  // список букв и их частотность
+      /* переменные */ 
+      // чтение исходного файла
+      var contentInput = File.ReadAllText($"{Path}{fileName}");
+      // длина исходного файла
+      var lengthContentInput = contentInput.Length;
+      // список букв и их количество
+      var countingContentInput = new Dictionary<string, double>();
+      // список букв и их частотность
+      var countingFrequencyContentInput = new Dictionary<string, double>();
+      // присваивание русского алфавита
+      var listLetter = File.ReadAllText($"{Path}Alphabet.txt");
+      // список удаляемых символов
+      var deletedSymbols = new List<string>();                               
 
       // посчёт символов
       foreach (var sym in contentInput) 
@@ -36,8 +44,10 @@ namespace secondTask
       Console.WriteLine();
       
       // вывод символов и их количества в тексте
+      Console.WriteLine("Вывод символов и их количества в тексте: ");
       foreach (var sym in countingContentInput)
         Console.WriteLine(sym.Key + " " + sym.Value);
+      Console.WriteLine();
       
       // подсчёт частотности
       foreach (var sym in countingContentInput)
@@ -47,22 +57,46 @@ namespace secondTask
       }
 
       // вывод частотности символов
-      Console.WriteLine();
+      Console.WriteLine("Вывод частотности символов: ");
       foreach (var sym in countingFrequencyContentInput)
         Console.WriteLine(sym.Key + " " + sym.Value);
+      Console.WriteLine();
 
       // сортировка символов
       var listFrequencyContent = countingFrequencyContentInput
                                                   .OrderBy(e => e.Value).Reverse();
-      
       // добавление в словарь
       var sortingCountingFrequencyContentInput = listFrequencyContent
                                                     .ToDictionary(x => x.Key, x => x.Value);
 
       // вывод отсортированного списка частотности
-      Console.WriteLine();
+      Console.WriteLine("Вывод отсортированного списка частотности: ");
       foreach (var sym in sortingCountingFrequencyContentInput)
         Console.WriteLine(sym.Key + " " + sym.Value);
+      
+      
+      // добавление удаляемых символов в список
+      foreach (var sym in sortingCountingFrequencyContentInput)
+        if (!listLetter.Contains(sym.Key) || sym.Key == "") 
+          deletedSymbols.Add(sym.Key);
+      
+      // вывод удаляемых символов
+      Console.WriteLine("Вывод удаляемых символов: ");
+      foreach (var sym in deletedSymbols)
+        Console.WriteLine(sym);
+      Console.WriteLine();
+            
+      // удаление символов, не являющимися буквами
+      foreach (var sym in deletedSymbols)
+        sortingCountingFrequencyContentInput.Remove(sym);
+            
+      // вывод частотности символов после удаления символов
+      Console.WriteLine("Вывод частотности символов после удаления символов: ");
+      foreach (var sym in sortingCountingFrequencyContentInput)
+        Console.WriteLine(sym.Key + " " + sym.Value);
+      Console.WriteLine();
+      
+      Console.WriteLine("---------------");
       
       return sortingCountingFrequencyContentInput;
     }
@@ -70,21 +104,19 @@ namespace secondTask
     // decryption method
     private static string s_decryptionMethod()
     {
+      /* переменные */
+      // чтение изходного текста
       var inputText = File.ReadAllText($"{Path}input2.txt");
+      // подсчёт частотности исходного текста
       var resultFrequencyMethod = s_frequencyMethod("input2.txt");
-      var listLetter = File.ReadAllText($"{Path}Alphabet.txt");
-      var listLetterFrequencies = File.ReadAllText($"{Path}AlphabetFrequencies.txt").ToList(); 
-      var deletedSymbols = new List<string>();
+      // присваивания алфавита по частотности
+      var listLetterFrequencies = File.ReadAllText($"{Path}AlphabetFrequencies.txt").ToList();
+      // список сапоставления символов
       var requenceiesLettersToInput = new Dictionary<string, string>();
+      // строка результата
       var resultDecryptionInputText = string.Empty;
-
-      foreach (var sym in resultFrequencyMethod)
-        if (!listLetter.Contains(sym.Key) || sym.Key == "") 
-           deletedSymbols.Add(sym.Key);
-
-      foreach (var sym in deletedSymbols)
-        resultFrequencyMethod.Remove(sym);
       
+      // добавление символов в список сапоставления
       foreach (var sym in resultFrequencyMethod)
       {
         foreach (var symbol in listLetterFrequencies)
@@ -98,9 +130,13 @@ namespace secondTask
         }
       }
 
+      // вывод списка сапоставления
+      Console.WriteLine("Вывод списка сапоставления: ");
       foreach (var sym in requenceiesLettersToInput)
         Console.WriteLine(sym.Key + " " + sym.Value);
+      Console.WriteLine();
       
+      // добавление в строку результата
       foreach (var sym in inputText)
       {
         //Console.WriteLine(sym);
@@ -114,9 +150,10 @@ namespace secondTask
         else resultDecryptionInputText += sym; 
       }
         
+      // вывод результата
       Console.WriteLine(resultDecryptionInputText);
 
-      return "str";
+      return resultDecryptionInputText;
     }
 
     /* Main method */
