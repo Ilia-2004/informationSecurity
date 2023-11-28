209047291885db86e9959687e573eeffd500b46c
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-11-28 10:58:06 +0400

    Added the decryption method in four solution

--- fourTask/Program.cs
+++ fourTask/Program.cs
@@ -27,15 +27,42 @@ internal abstract class Program
       if (keyIndex >= contentKey.Length) keyIndex = 0;
     }
 
+    using (var sw = new StreamWriter($"{Path}Out.txt")) { sw.Write(outputText.ToLower()); }
+
     return outputText;
   }
 
- 
+  // decryption method
+  private static string s_decryptionMethod(string nameFile)
+  {
+    var contentKey = File.ReadAllText($"{Path}Key.txt");
+    var contentInputText = s_encryptionMethod(nameFile);
+    var resultText = string.Empty;
+    var keyIndex = 0; 
+    
+    for (int i = 0; i < contentInputText.Length; i++)
+    {
+      var a = (int)contentInputText[i];
+      var b = (int)contentKey[keyIndex];
+      int c = ((a & b) | (~a & ~b)) % 32 + 'а';
+      
+      resultText += (char)c;   
+      keyIndex++;
+
+      if (keyIndex >= contentKey.Length) keyIndex = 0;
+    }
+
+    using (var sw = new StreamWriter($"{Path}Result.txt")) { sw.Write(resultText.ToLower()); }
+
+    return resultText;
+  }
 
   /* Main method */
   public static void Main()
   {
     var inputText = "Input.txt";
-    s_encryptionMethod(inputText);
+    Console.WriteLine(s_encryptionMethod(inputText));
+    Console.WriteLine();
+    Console.WriteLine(s_decryptionMethod(inputText));
   }
 }
